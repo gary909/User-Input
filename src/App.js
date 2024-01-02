eee84d7942e58b4476690d3b4f6f91ec4a67b090
whg <whg@makrosolutions.com>
2024-01-02 11:19:04 +0100

    app working

--- src/App.js
+++ src/App.js
@@ -1,23 +1,54 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
 
 function App() {
+  // State variables for first name and last name
+  const [fName, setFName] = useState("");
+  const [lName, setLName] = useState("");
+  const [eMail, setEmail] = useState("");
+
+  // Function to handle changes in the first name input
+  const handleFNameChange = (event) => {
+    setFName(event.target.value);
+  };
+
+  // Function to handle changes in the last name input
+  const handleLNameChange = (event) => {
+    setLName(event.target.value);
+  };
+
+  // Function to handle changes in the last name input
+  const handleEmailChange = (event) => {
+    setEmail(event.target.value);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <h1>
+        Hello {fName} {lName}
+      </h1>
+      <h3>{eMail}</h3>
+      <br></br>
+      <form>
+        <input
+          name="fName"
+          placeholder="First Name"
+          value={fName}
+          onChange={handleFNameChange}
+        />
+        <input
+          name="lName"
+          placeholder="Last Name"
+          value={lName}
+          onChange={handleLNameChange}
+        />
+        <input
+          name="eMail"
+          placeholder="E-Mail"
+          value={eMail}
+          onChange={handleEmailChange}
+        />
+        <button>Submit</button>
+      </form>
     </div>
   );
 }
